ff92ff58d3be509ca0b01662e9781fe3b0b6e46e
habner-gabryel <habner.gabryel.cc@gmail.com>
2021-10-11 15:01:41 -0300

    Adicionada classe position

--- src/application/Program.java
+++ src/application/Program.java
@@ -3,12 +3,16 @@ package application;
 import java.util.Locale;
 import java.util.Scanner;
 
+import boardgame.Position;
+
 public class Program {
 
 	public static void main(String[] args) {
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
 		
+		Position pos = new Position(3, 5);
+		System.out.println(pos);
 		
 		sc.close();
 	}
