737c200f4707d4903e012ace8de97f91eb5f5d13
habner-gabryel <habner.gabryel.cc@gmail.com>
2021-10-11 15:09:55 -0300

    Adicionadas classes piece e board.

--- src/application/Program.java
+++ src/application/Program.java
@@ -3,7 +3,7 @@ package application;
 import java.util.Locale;
 import java.util.Scanner;
 
-import boardgame.Position;
+import boardgame.Board;
 
 public class Program {
 
@@ -11,8 +11,7 @@ public class Program {
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
 		
-		Position pos = new Position(3, 5);
-		System.out.println(pos);
+		Board board = new Board(8,8);
 		
 		sc.close();
 	}
---
+++ src/xadrez_project/Teste.java
@@ -0,0 +1,7 @@
+package xadrez_project;
+
+public class Teste {
+	public static void main (String[] args) {
+		System.out.println("Teste git");
+	}
+}
