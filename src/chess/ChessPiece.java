7dc185cd76e1e0e5ecb611dafeba5a384c5e37be
habner-gabryel <habner.gabryel.cc@gmail.com>
2021-10-13 11:26:14 -0300

    Implementada a limitação de movimentos possiveis para as peças.

--- src/chess/ChessPiece.java
+++ src/chess/ChessPiece.java
@@ -3,7 +3,7 @@ package chess;
 import boardgame.Board;
 import boardgame.Piece;
 
-public class ChessPiece extends Piece{
+public abstract class ChessPiece extends Piece{
 	private Color color;
 
 	public Color getColor() {
@@ -14,6 +14,5 @@ public class ChessPiece extends Piece{
 		super(board);
 		this.color = color;
 	}
-	
 	
 }
