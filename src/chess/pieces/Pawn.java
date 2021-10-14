7c9809674438c0adbc44e911b1135a560f1a7ea7
habner-gabryel <habner.gabryel.cc@gmail.com>
2021-10-14 17:54:49 -0300

    Adicionada rainha

--- src/chess/pieces/Pawn.java
+++ src/chess/pieces/Pawn.java
@@ -23,7 +23,7 @@ public class Pawn extends ChessPiece {
 		Position p = new Position(0, 0);
 		
 		if (getColor() == Color.WHITE) {
-			p.setValues(position.getRow() -1, position.getColumn());
+			p.setValues(position.getRow() - 1, position.getColumn());
 			if (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)) {
 				mat[p.getRow()][p.getColumn()] = true;
 			}
@@ -45,12 +45,12 @@ public class Pawn extends ChessPiece {
 				mat[p.getRow()][p.getColumn()] = true;
 			}
 		}else {
-			p.setValues(position.getRow() +1, position.getColumn());
+			p.setValues(position.getRow() + 1, position.getColumn());
 			if (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)) {
 				mat[p.getRow()][p.getColumn()] = true;
 			}
 			
-			p.setValues(position.getRow() +2, position.getColumn());
+			p.setValues(position.getRow() + 2, position.getColumn());
 			Position p2 = new Position(position.getRow() + 1, position.getColumn());
 			if (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p) &&
 				getBoard().positionExists(p2) && !getBoard().thereIsAPiece(p2) && getMoveCount() == 0) {
